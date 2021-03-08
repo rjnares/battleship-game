a1e08f0af9a6871d66473e2448a9f0ef386a1495
rjnares <roberto.nares@gmail.com>
2021-03-07 22:29:00 -0800

    Renamed 'computer' references to 'ai' in 'app.js', 'index.html', and 'style.css'. Created function that generates all AI ships in random locations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const userGrid = document.querySelector(".grid-user");
-  const computerGrid = document.querySelector(".grid-computer");
+  const aiGrid = document.querySelector(".grid-ai");
   const chooseShipsGrid = document.querySelector(".grid-choose-ships");
   const ships = document.querySelectorAll(".ship");
   const carrier = document.querySelector(".carrier");
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const currentTurnDisplay = document.querySelector("#current-turn");
   const gameInfoDisplay = document.querySelector("#game-info");
   const userSquares = [];
-  const computerSquares = [];
+  const aiSquares = [];
   const width = 10;
 
   // Create game board
@@ -27,5 +27,95 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   createBoard(userGrid, userSquares);
-  createBoard(computerGrid, computerSquares);
+  createBoard(aiGrid, aiSquares);
+
+  // Ships
+  const shipArray = [
+    {
+      name: "carrier",
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, 2 * width, 3 * width, 4 * width],
+      ],
+    },
+    {
+      name: "battleship",
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, 2 * width, 3 * width],
+      ],
+    },
+    {
+      name: "cruiser",
+      directions: [
+        [0, 1, 2],
+        [0, width, 2 * width],
+      ],
+    },
+    {
+      name: "submarine",
+      directions: [
+        [0, 1, 2],
+        [0, width, 2 * width],
+      ],
+    },
+    {
+      name: "destroyer",
+      directions: [
+        [0, 1],
+        [0, width],
+      ],
+    },
+  ];
+
+  function isValidStart(start, shipLength, direction) {
+    if (direction) {
+      // if vertical, only need to check upper limit on start cell
+      const upperLimit = 99 - (shipLength - 1) * width;
+
+      return start <= upperLimit;
+    } else {
+      // if horizontal, need to check the mod of upper limit since it varies for each row
+      const upperModLimit = width - shipLength;
+      const startModValue = start % width;
+
+      return startModValue <= upperModLimit;
+    }
+  }
+
+  function isFilled(start, direction) {
+    // check if any cells of current ship are already filled by another ship
+    return direction.some((index) =>
+      aiSquares[start + index].classList.contains("filled")
+    );
+  }
+
+  // Generate random AI ship layout
+  function generateRandomAiShipLayout(ship) {
+    const shipIndexes = ship.directions;
+    const shipLength = shipIndexes[0].length;
+
+    // Chooses vertical or horizontal placement randomly
+    const randomDirection = Math.floor(Math.random() * 2);
+    const currentDirection = shipIndexes[randomDirection];
+
+    // Choose random start from cells 0 - 99
+    let randomStart = Math.floor(Math.random() * 100);
+
+    // While valid random start is not found OR cell(s) already taken, generate new one
+    while (
+      !isValidStart(randomStart, shipLength, randomDirection) ||
+      isFilled(randomStart, currentDirection)
+    ) {
+      // Choose NEW random start from cells 0 - 99
+      randomStart = Math.floor(Math.random() * 100);
+    }
+
+    // If here, then we have both valid random start and no cells are already filled
+    currentDirection.forEach((index) =>
+      aiSquares[randomStart + index].classList.add("filled", ship.name)
+    );
+  }
+
+  shipArray.forEach((ship) => generateRandomAiShipLayout(ship));
 });
