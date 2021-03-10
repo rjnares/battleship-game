a06658b0bd4066cd86ada5a0fa2161cef5bd413e
rjnares <roberto.nares@gmail.com>
2021-03-09 22:03:40 -0800

    Server now serves static game files on a specified port (default 3000). Can play game at {server host}:3000

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Hello Server");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+const socketio = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Static folder: folder server will 'serve' to client (this is public folder w/game files)
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
